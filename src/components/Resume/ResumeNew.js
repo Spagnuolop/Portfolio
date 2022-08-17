405168d70e0d0e4dcdeef40222c42ea0e7865c4e
Pedro <spagnuolopedro@hotmail.com>
2022-08-17 02:10:57 -0300

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/Spagnuolop/Portfolio/blob/main/src/Assets/Curriculum.pdf";
+  "raw.githubusercontent.com/Spagnuolop/Portfolio/main/src/Assets/Curriculum.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
